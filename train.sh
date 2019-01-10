7e1657a197152167c1a5dcd9243ff86209ff3d00
coast <Coastchb@sina.com>
2019-01-10 21:03:44 +0800

    train.sh modified

--- train.sh
+++ train.sh
@@ -8,12 +8,27 @@ fi
 stage=$1
 speaker=$2
 
-# extract acoustic feature (including dynamic features)
+# extract acoustic feature (without dynamic features)
 if [ $stage -le 1 ];then
   python local/extract_acoustic_feat.py --br bin/ --dr data/$speaker
 fi
 
-# copy synthesis
+# copy synthesis to verify extracted acoustic features
 if [ $stage -le 2 ];then
   python local/copy_syn.py --br bin/ --dr data/$speaker
+fi
+
+# preprocess acoustic feature and text(lingual features)
+if [ $stage -le 3 ];then
+  python preprocess.py
+fi
+
+# train model
+if [ $stage -le 4 ];then
+  python train.py
+fi
+
+# synthesis
+if [ $stage -le 5 ];then
+
 fi
